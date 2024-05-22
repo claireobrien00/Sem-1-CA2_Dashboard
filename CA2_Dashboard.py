0e201143f3ee740ea89594dd10a785cb00adb5ed
clairobrien00 <sbs24031@student.cct.ie>
2024-05-22 13:46:00 +0100

    q

--- CA2_Dashboard.py
+++ CA2_Dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # Title of the app
 st.title('Data Visualization Dashboard')
@@ -29,53 +28,3 @@ st.dataframe(df)
 st.header('Data Analysis')
 st.write("Select columns to visualize")
 
-columns = st.multiselect("Select columns", df.columns)
-
-if columns:
-    st.write(df[columns])
-
-    # Correlation matrix
-    st.subheader('Correlation Matrix')
-    correlation_matrix = df[columns].corr()
-    st.write(correlation_matrix)
-    fig, ax = plt.subplots()
-    sns.heatmap(correlation_matrix, annot=True, ax=ax)
-    st.pyplot(fig)
-
-    # Pairplot
-    st.subheader('Pairplot')
-    pairplot = sns.pairplot(df[columns])
-    st.pyplot(pairplot)
-
-# Bar chart
-st.header('Bar Chart')
-selected_column = st.selectbox("Select column for bar chart", df.columns)
-if selected_column:
-    bar_data = df[selected_column].value_counts()
-    st.bar_chart(bar_data)
-
-# Line chart
-st.header('Line Chart')
-selected_line_columns = st.multiselect("Select columns for line chart", df.columns)
-if selected_line_columns:
-    st.line_chart(df[selected_line_columns])
-
-# Area chart
-# st.header('Area Chart')
-# selected_area_columns = st.multiselect("Select columns for area chart", df.columns, default=df.columns[:2])
-# if selected_area_columns:
-#     st.area_chart(df[selected_area_columns])
-
-# Custom plot with Matplotlib/Seaborn
-st.header('Custom Plot')
-custom_plot_column = st.selectbox("Select column for custom plot", df.columns)
-if custom_plot_column:
-    fig, ax = plt.subplots()
-    sns.histplot(df[custom_plot_column], bins=30, kde=True, ax=ax)
-    st.pyplot(fig)
-
-# Instructions to run the app
-st.write("To run this app, use the command: `streamlit run dashboard.py`")
-
-# Additional information
-st.write("For more information, visit the [Streamlit documentation](https://docs.streamlit.io/).")
