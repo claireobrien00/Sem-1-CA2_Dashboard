09ca511247f8fb0d52cf3686c7a536939ff62926
clairobrien00 <sbs24031@student.cct.ie>
2024-05-22 13:38:34 +0100

    q

--- CA2_Dashboard.py
+++ CA2_Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
+import seaborn as sns
 
 # Title of the app
 st.title('Data Visualization Dashboard')
